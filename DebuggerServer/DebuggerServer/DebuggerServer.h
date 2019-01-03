fe330cb6e18fa4e966a8cb671540585ecfd49d1c
Kirill Ushkov <kirill.u@itomy.ch>
2019-01-03 17:27:52 +0200

    Updated project structure

---
+++ DebuggerServer/DebuggerServer/DebuggerServer.h
@@ -0,0 +1,19 @@
+//
+//  DebuggerServer.h
+//  DebuggerServer
+//
+//  Created by Kirill Ushkov on 03.01.19.
+//  Copyright © 2019 itomy.ch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DebuggerServer.
+FOUNDATION_EXPORT double DebuggerServerVersionNumber;
+
+//! Project version string for DebuggerServer.
+FOUNDATION_EXPORT const unsigned char DebuggerServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DebuggerServer/PublicHeader.h>
+
+
--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-# Swift Talk
-## iOS Remote Debugger: Sending Data
-
-This is the code that accompanies Swift Talk Episode 110: [iOS Remote Debugger: Sending Data](https://talk.objc.io/episodes/S01E110-ios-remote-debugger-sending-data)
+Reimplementation of remote debugger: [iOS Remote Debugger: Sending Data](https://talk.objc.io/episodes/S01E110-ios-remote-debugger-sending-data)
